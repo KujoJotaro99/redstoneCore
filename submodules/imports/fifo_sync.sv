89bb3b967bc0607fbae84cfe4beca836bdc13067
KujoJotaro99 <mhari2041@gmail.com>
2026-01-16 15:24:00 -0800

    rtl: add reusable memory primitives

---
+++ submodules/imports/fifo_sync.sv
@@ -0,0 +1,88 @@
+`timescale 1ns/1ps
+
+module fifo_sync 
+#(
+    parameter WIDTH_P = 8,
+    parameter DEPTH_P = 16
+) (
+    input logic [0:0] clk_i,
+    input logic [0:0] rstn_i,
+    input logic [0:0] flush_i,
+    input logic [WIDTH_P-1:0] data_i,
+    input logic [0:0] valid_i,
+    input logic [0:0] ready_i,
+    output logic [0:0] valid_o,
+    output logic [0:0] ready_o,
+    output logic [WIDTH_P-1:0] data_o
+);
+
+    logic [$clog2(DEPTH_P):0] wr_ptr_l, rd_ptr_l, rd_ptr_next_w;
+    logic [WIDTH_P-1:0] data_o_bypass_l, data_o_l;
+    logic [0:0] bypass_w;
+
+    // full empty and bypass logic
+    assign ready_o = ~((wr_ptr_l[$clog2(DEPTH_P)] != rd_ptr_l[$clog2(DEPTH_P)]) && (wr_ptr_l[$clog2(DEPTH_P)-1:0] == rd_ptr_l[$clog2(DEPTH_P)-1:0])); // not full
+    assign valid_o = (wr_ptr_l[$clog2(DEPTH_P):0] != rd_ptr_l[$clog2(DEPTH_P):0]); // not empty
+    assign bypass_w = (wr_ptr_l[$clog2(DEPTH_P):0] == rd_ptr_next_w[$clog2(DEPTH_P):0]);
+
+    // next ptr logic
+    always_comb begin
+        if (!rstn_i) begin
+            rd_ptr_next_w = '0;
+        end else if (valid_o & ready_i) begin
+            rd_ptr_next_w = rd_ptr_l + 1'b1;
+        end else begin
+            rd_ptr_next_w = rd_ptr_l;
+        end
+    end
+
+    // curr ptr logic
+    always_ff @(posedge clk_i) begin
+        if (!rstn_i) begin
+            wr_ptr_l <= '0;
+        end else if (flush_i) begin
+            wr_ptr_l <= '0;
+        end else if (valid_i & ready_o) begin
+            wr_ptr_l <= wr_ptr_l + 1'b1;
+        end
+    end
+
+    always_ff @(posedge clk_i) begin
+        if (!rstn_i) begin
+            rd_ptr_l <= '0;
+        end else if (flush_i) begin
+            rd_ptr_l <= '0;
+        end else if (valid_o & ready_i) begin
+            rd_ptr_l <= rd_ptr_l + 1'b1;
+        end
+    end
+
+    always_ff @(posedge clk_i) begin
+        if (!rstn_i) begin
+            data_o_bypass_l <= '0;
+        end else if (flush_i) begin
+            data_o_bypass_l <= '0;
+        end else if (valid_i & ready_o) begin
+            data_o_bypass_l <= data_i;
+        end
+    end
+
+    // sync ram
+    sync_ram_block #(
+        .WIDTH_P(WIDTH_P),
+        .DEPTH_P(DEPTH_P)
+    ) sync_fifo_ram (
+        .clk_i(clk_i),
+        .rstn_i(rstn_i),
+        .data_i(data_i),
+        .wr_addr_i(wr_ptr_l[$clog2(DEPTH_P)-1:0]),
+        .rd_addr_i(rd_ptr_next_w[$clog2(DEPTH_P)-1:0]),
+        .wr_en_i(valid_i & ready_o),
+        .wr_mask_i({(WIDTH_P/8){1'b1}}),
+        .rd_en_i(1'b1),
+        .data_o(data_o_l)
+    );
+
+    assign data_o = bypass_w ? data_o_bypass_l : data_o_l;
+
+endmodule
---
+++ submodules/imports/sync_ram_block.sv
@@ -0,0 +1,53 @@
+`timescale 1ns/1ps
+
+module sync_ram_block #(
+    parameter WIDTH_P = 32, 
+    parameter DEPTH_P = 128, 
+    parameter filename_p = ""
+) (
+    input logic [0:0] clk_i,
+    input logic [0:0] rstn_i,
+    input logic [WIDTH_P-1:0] data_i,
+    input logic [$clog2(DEPTH_P)-1:0] wr_addr_i,
+    input logic [$clog2(DEPTH_P)-1:0] rd_addr_i,
+    input logic [0:0] wr_en_i,
+    input logic [(WIDTH_P/8)-1:0] wr_mask_i,
+    input logic [0:0] rd_en_i,
+    output logic [WIDTH_P-1:0] data_o
+);
+
+    logic [WIDTH_P-1:0] mem_array [DEPTH_P-1:0];
+    integer i;
+    integer byte_idx;
+
+    initial begin
+        if (filename_p != "") begin
+            $readmemh(filename_p, mem_array, 0, DEPTH_P-1);
+        end
+// `ifndef SYNTHESIS
+//         for (i = 0; i < DEPTH_P; i = i + 1) begin
+//             $dumpvars(0, mem_array[i]);
+//         end
+// `endif
+`ifndef SYNTHESIS
+        $display("%m: depth_p is %d, width_p is %d", DEPTH_P, WIDTH_P);
+`endif
+    end
+
+    always_ff @(posedge clk_i) begin
+        if (!rstn_i) begin
+
+        end else begin
+            if (rd_en_i) begin
+                data_o <= mem_array[rd_addr_i];
+            end
+            if (wr_en_i) begin
+                for (byte_idx = 0; byte_idx < WIDTH_P/8; byte_idx = byte_idx + 1) begin
+                    if (wr_mask_i[byte_idx]) begin
+                        mem_array[wr_addr_i][byte_idx*8 +: 8] <= data_i[byte_idx*8 +: 8];
+                    end
+                end
+            end
+        end
+    end
+endmodule
